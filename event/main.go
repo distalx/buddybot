a60caa74882e6b9d660c6c3dd31b139e8405ad21
Bill Glover <bill@billglover.co.uk>
2018-08-07 09:11:50 +0100

    Fix: improve readability of increment functionality
    Fixes #19

--- event/main.go
+++ event/main.go
@@ -7,16 +7,16 @@ import (
 	"os"
 	"regexp"
 
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/billglover/buddybot/bot"
 	"github.com/nlopes/slack"
 	"github.com/nlopes/slack/slackevents"
+	"github.com/pkg/errors"
 )
 
 func main() {
@@ -33,7 +33,6 @@ func handler(b *bot.SlackBot) bot.APIHandler {
 
 	return func(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-		// parse the event
 		e, err := b.ParseEvent(req)
 		if err != nil {
 			fmt.Println("WARN: failed to parse request:", err)
@@ -43,14 +42,12 @@ func handler(b *bot.SlackBot) bot.APIHandler {
 
 		switch e.Type {
 
-		// We must respond to URLVerification events to allow us to add the endpoint in
-		// the Slack App.
 		case slackevents.URLVerification:
-			fmt.Println("INFO: event type:", e.Type)
+
 			var r *slackevents.ChallengeResponse
 			err := json.Unmarshal([]byte(req.Body), &r)
 			if err != nil {
-				fmt.Println("WARN: failed to parse challenge body:", err)
+				fmt.Println("WARN: failed to parse URL Verification challenge:", err)
 				resp := events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest}
 				return resp, nil
 			}
@@ -62,21 +59,18 @@ func handler(b *bot.SlackBot) bot.APIHandler {
 			}
 			return resp, nil
 
-		// The CallbackEvent type contains inner events which we need to differentiate
-		// before we can do anything useful.
 		case slackevents.CallbackEvent:
 			cbe := e.Data.(*slackevents.EventsAPICallbackEvent)
-			switch ev := e.InnerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
 
-				// retrieve the appropriate bot token
+			switch ev := e.InnerEvent.Data.(type) {
+
+			case *slackevents.AppMentionEvent:
 				token, err := b.RetrieveToken(cbe.TeamID)
 				if err != nil {
-					fmt.Println("WARN: unable to retrieve access token:", err)
+					fmt.Println("WARN: unable to retrieve team access token:", err)
 					resp := events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}
 					return resp, nil
 				}
-
 				api := slack.New(token)
 
 				plusUsers := identifyPlusPlus(ev.Text)
@@ -84,6 +78,7 @@ func handler(b *bot.SlackBot) bot.APIHandler {
 				for _, u := range plusUsers {
 					params := slack.PostMessageParameters{}
 
+					// Don't let users boost their own egos
 					if u == ev.User {
 						reply := fmt.Sprintf("No <@%s>, try patting yourself on the back instead :stuck_out_tongue_closed_eyes:", u)
 						_, _, err := api.PostMessage(ev.Channel, reply, params)
@@ -93,42 +88,13 @@ func handler(b *bot.SlackBot) bot.APIHandler {
 						break
 					}
 
-					// TODO: update score in database
-
-					sess, err := session.NewSession(&aws.Config{Region: aws.String(b.Region)})
+					score, err := incrementScore(b, cbe.TeamID, u)
+					reply := fmt.Sprintf("Congrats <@%s>! Score now at %d :smile:", u, score)
 					if err != nil {
-						fmt.Println("ERROR: unable to create session:", err)
-						break
+						fmt.Println("WARN: unable to increment score:", err)
+						reply = fmt.Sprintf("Congrats <@%s>! I was unable to update your score, so you'll have to accept this smile instead :smile:", u)
 					}
 
-					ddb := dynamodb.New(sess)
-
-					input := &dynamodb.UpdateItemInput{
-						ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{":s": {N: aws.String("1")}},
-						TableName:                 aws.String(b.ScoreTable),
-						Key:                       map[string]*dynamodb.AttributeValue{"uid": {S: aws.String(cbe.TeamID + ":" + u)}},
-						ReturnValues:              aws.String("UPDATED_NEW"),
-						UpdateExpression:          aws.String("add score :s"),
-					}
-
-					v, err := ddb.UpdateItem(input)
-					if err != nil {
-						fmt.Println("ERROR: unable to update database:", err)
-						break
-					}
-
-					fmt.Printf("INFO: %+v\n", v)
-					fmt.Printf("INFO: %+v\n", v.Attributes["score"])
-
-					record := new(int)
-					err = dynamodbattribute.Unmarshal(v.Attributes["score"], record)
-
-					if err != nil {
-						fmt.Println("ERROR: unable to unmarshal return value:", err)
-						break
-					}
-
-					reply := fmt.Sprintf("Congrats <@%s>! Score now at %d :smile:", u, *record)
 					_, _, err = api.PostMessage(ev.Channel, reply, params)
 					if err != nil {
 						fmt.Println("WARN: unable to post message:", err)
@@ -153,4 +119,39 @@ func identifyPlusPlus(msg string) []string {
 		users = append(users, string(match[1]))
 	}
 	return users
+}
+
+// IncrementScore takes a team and a user an increments the score by one. It returns the
+// new score or an error.
+func incrementScore(b *bot.SlackBot, team, user string) (int, error) {
+
+	score := 0
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(b.Region)})
+	if err != nil {
+		return score, errors.Wrap(err, "unable to create session")
+	}
+
+	ddb := dynamodb.New(sess)
+
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{":s": {N: aws.String("1")}},
+		TableName:                 aws.String(b.ScoreTable),
+		Key:                       map[string]*dynamodb.AttributeValue{"uid": {S: aws.String(team + ":" + user)}},
+		ReturnValues:              aws.String("UPDATED_NEW"),
+		UpdateExpression:          aws.String("add score :s"),
+	}
+
+	v, err := ddb.UpdateItem(input)
+	if err != nil {
+		return score, errors.Wrap(err, "unable to update database")
+	}
+
+	err = dynamodbattribute.Unmarshal(v.Attributes["score"], &score)
+
+	if err != nil {
+		return score, errors.Wrap(err, "unable to unmarshal return value")
+	}
+
+	return score, nil
 }
